34db86deae2eb621b0a4122bbe8229c4f5412519
Matt Kingston <mattkingston@gmail.com>
2021-02-27 16:38:07 +0000

    Replace timescale with simplified timescale (#1)
    * Remove old timescale
    
    * Restore timescale with simplified timescale

--- timescale/scripts/pgbackrest_archive_get.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This file and its contents are licensed under the Apache License 2.0.
-# Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
-PGBACKREST_BACKUP_ENABLED=0
-[ ${PGBACKREST_BACKUP_ENABLED} -ne 0 ] || exit 0
-
-. "${HOME}/.pgbackrest_environment"
-exec pgbackrest --stanza=poddb archive-get "${1}" "${2}"
